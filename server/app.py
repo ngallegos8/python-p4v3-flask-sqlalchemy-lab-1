976550a135d221000ebff7914a25b7b7563bc28d
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2024-02-22 11:41:06 -0700

    4/6 tests pass

--- server/app.py
+++ server/app.py
@@ -21,6 +21,45 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id==id).first()
+
+    if earthquake:
+        body = {'id': earthquake.id,
+                'location': earthquake.location,
+                'magnitude': earthquake.magnitude,
+                'year': earthquake.year
+                }
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+
+    return make_response(body, status)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_by_magnitude(magnitude):
+    earthquake = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    if earthquake:
+        body = {
+            'count': len(earthquake),
+            'quakes': [
+                {
+                'id': earthquake.id,
+                'location': earthquake.location,
+                'magnitude': earthquake.magnitude,
+                'year': earthquake.year
+                },
+            ]
+        }
+        status = 200
+    else:
+        body = {'message': f'Earthquake {magnitude} not found.'}
+        status = 404
+
+    return make_response(body, status)
 
 
 if __name__ == '__main__':
